e3ccc6f83cc8b8f5806f273818b630cb75e33730
Arsal Fadilah <arsal.fadilah.tif420@polban.ac.id>
2021-08-08 08:33:04 +0700

    add typewriter effect

--- assets/javascript/kode.js
+++ assets/javascript/kode.js
@@ -1,3 +1,64 @@
+/* Typewriter */
+// writing text
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+$(window).on('load', function() {
+    //writing text
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+});
+
+
 function addBorder() {
     var x = document.getElementById("toggler");
     // console.log(x.attributes[6].value)
@@ -17,27 +78,27 @@ function validationSignUp() {
     var pass = document.getElementById("pass").value;
     var cpass = document.getElementById("cpass").value;
 
-    if(pass.length < 8){
+    if (pass.length < 8) {
         alert("Your password to short!");
         submitOK = "false";
     }
 
-    if(pass!=cpass){
+    if (pass != cpass) {
         alert("Your confirm password not same!");
         submitOK = "false";
     }
-    
+
     if (submitOK == "false") {
         return false;
     }
 }
 
-function responsiveNavIcon(icon){
-    if(icon.matches){
-        document.getElementById("text-icon").innerHTML=`<img src="./assets/img/logo.png" alt="logo" width="30"
+function responsiveNavIcon(icon) {
+    if (icon.matches) {
+        document.getElementById("text-icon").innerHTML = `<img src="./assets/img/logo.png" alt="logo" width="30"
         height="24" class="d-inline-block align-text-top">`;
-    }else{
-        document.getElementById("text-icon").innerHTML =`<img src="./assets/img/logo.png" alt="logo" width="30"
+    } else {
+        document.getElementById("text-icon").innerHTML = `<img src="./assets/img/logo.png" alt="logo" width="30"
         height="24" class="d-inline-block align-text-top">
     Programming Nusantara`;
     }
